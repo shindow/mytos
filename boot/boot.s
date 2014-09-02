2add5aa184be0d41b5d723c777e563e348d0782f
Ubuntu-12.04 <shindow@ubuntu.(none)>
2014-09-01 19:13:43 -0700

    boot files

--- boot/boot.s
+++ boot/boot.s
@@ -1,54 +1,178 @@
 ;       booter								
 ;	by shindow(liuyu_200411@sina.com) 				
-;	2010.1.13  setup		  												
-org 0x7c00
-mov ax,cs
-mov es,ax
-mov ax,0
-mov ss,ax
-mov ds,ax
-mov esp,0x1000 ;设置寄存器
+;	2010.1.13  setup
 
-call ReadSector
-;获得需要读取的扇区
-mov cx,word [0x7c00+0x240+2]	;load.bin 多少扇区数
-;加载load.bin
-Load:
+;	rewrite 2014.8.28  liuyu
+
+;org 0x7c00,这会把所有的代码地址符都加一个0x7c00,如果不加这个文件会导致的问题是，如jmp Load
+;Load的地址在没加org0x7c00时假设是0x30，所以jmp Load，不会跳转到Load，因为正真的Load在0x7c30
+;故如果不是程序作了如下ds，cs等设置，会出现一些问题。
+;(if we don't init cs,ds and so on,it will cause problem such as jmp Load instruction,cause without org 0x7c00
+;the address of Load is like 0x30,but with the org 0x7c00 instruction,it will be 0x7c30,actually the Load is at
+;0x7c30,but if you set the cs to 0x7c0,the condition is opposite.with the org 0x7c00,jmp Load will be like this:
+;jmp 0x7c0:0x7c30,it will be wrong absolutely.)
+;初始化寄存器(init registers)
+
+;有时需要切换ds，因为访问其他内存空间时，如访问0x78处，如果数据段是0x800显示访问不到相应的内存
+;所以此时需要把ds切换到0，而访问本代码里的变量时，显然应该把数据段切换到本程序加载的起始地址
+;sometimes it needs to exchange ds such as locate memory 0x78,if the ds is 0x800,it cannot locate memory 0x78
+;it needs to set ds to 0,and when locate variable in this file,it needs to set ds,such as 0x800
+
+	mov ax,cs
+	mov es,ax
+	
+	mov ax,STACK_SEG
+	mov ss,ax
+	mov eax,STACK_OFFSET
+	mov esp,eax
+;
+;拷贝引导扇区从0x7c00到0x8000(copy boot sector from 0x7c00 to 0x8000)
+;
+	mov ax,BOOT_START_SEG
+	mov ds,ax
+	mov ax,BOOT_NEW_SEG
+	mov es,ax
+	mov cx,512
+	sub si,si
+	sub di,di
+	rep movsb
+
+	jmp BOOT_NEW_SEG:restart
+
+restart:
+	;
+	;修改软驱参数(change the floppy parameter)
+	;
+	mov ax,0
+	mov ds,ax
+	mov es,ax
+	;软驱参数偏移地址(floppy drive parameter offset)
+	mov ax,word [0x78]
+	mov si,ax
+	;软驱参数段地址(floppy drive parameter segment)
+	mov ax,word [0x78+2]
+	mov ds,ax
+
+	mov ax,FLOPPY_TEMP_ADDR_OFFSET
+	mov di,ax
+	mov cx,12
+	rep movsb
+
+	;还原段寄存器
+	mov ax,0
+	mov ds,ax
+	mov es,ax
+
+	mov al,byte word [FLOPPY_TEMP_ADDR_OFFSET+4]
+	cmp al,0x12
+	;比较偏移4处的最大扇区数，如果是18则不做改变，否则重置软驱驱动器
+	;compare the 4th byte offset at 0x7c00,if it not equals 18,then reset this value to 18
+	jz NoChangeFloppyDriveParam
+	
+	mov byte word [FLOPPY_TEMP_ADDR_OFFSET+4],0x11
+	;重新指向软驱参数(repoint floppy parameter)
+	mov word [0x78],FLOPPY_TEMP_ADDR_OFFSET
+	mov word [0x78+2],FLOPPY_TEMP_ADDR_SEG
+
+	;恢复ds(reset ds to 0)
+	mov ax,0
+	mov ds,ax
+
+	sub ah,ah
+	sub dl,dl
+	int 0x13
+
+NoChangeFloppyDriveParam:
+	;重新设置段寄存器(reset segment register)
+	mov ax,BOOT_NEW_SEG
+	mov ds,ax
+	mov es,ax
+
+	;下面开始正真加载软盘中的内容
+	;begin to load the sectors in floppy
+
+
+	;设置加载内存地址(set the memAddr to be loaded)
+	mov word [copy_ds],BOOT_NEW_SEG
+	mov word [copy_off],0x200
+	;读取第二扇区,该扇区是头信息
+	call ReadASector
+	call IncSector
+
+	;获得需要读取load.bin的扇区数量(get the number of sectors for load.bin)
+	mov cx,word [0x240+2]	
+	;设置加载内存地址(set the memAddr to be loaded)
+	mov ax,LOAD_OFFSET
+	mov word [copy_off],ax
+	mov ax,LOAD_SEG
+	mov word [copy_ds],ax
+	;加载load.bin
+LoadLoadBin:
 	;循环度每一个扇区
 	push cx
+	call ReadASector
 	call IncSector
-	call ReadSector
 	pop cx
 	dec cx
 	cmp cx,0
-	jnz Load
-mov cx,word [0x7c00+0x280+2]	;kernel.bin 多少扇区数
-push cx
-mov cx,word [0x7c00+0x260+2]	;pic.bin 多少扇区数
+	jnz LoadLoadBin
 
-mov ax,0
-mov word [copy_off],ax
-mov ax,0x1000
-mov word [copy_ds],ax
+
+	;pic.bin 多少扇区数
+	mov cx,word [0x260+2]
+	;是否有PicBin
+	cmp cx,0
+	jz NoPicBin
+	;设置标志位为1
+	mov byte [bHasPicBin],1
+	;设置加载内存地址(set the memAddr to be loaded)
+	mov ax,PIC_OFFSET
+	mov word [copy_off],ax
+	mov ax,PIC_SEG
+	mov word [copy_ds],ax
 ;加载pic.bin
-Pic:		   
+LoadPicBin:		   
 	push cx
+	call ReadASector
 	call IncSector
-	call ReadSector
 	pop cx
 	dec cx
 	cmp cx,0
-	jnz Pic
-xor ax,ax
-mov al,[sector]
-push ax
-mov al,[tracker]
-push ax
-mov al,[header]
-push ax
-mov ax,[start]
-push ax
-jmp 0x800:0
+	jnz LoadPicBin
+NoPicBin:
+	;kernel.bin 多少扇区数
+	mov cx,word [0x280+2]	
+	;设置加载内存地址(set the memAddr to be loaded)
+	mov ax,KERNEL_OFFSET
+	mov word [copy_off],ax
+	mov ax,KERNEL_SEG
+	mov word [copy_ds],ax
+;加载kernel.bin
+LoadKernelBin:		   
+	push cx
+	call ReadASector
+	call IncSector
+	pop cx
+	dec cx
+	cmp cx,0
+	jnz LoadKernelBin
+	
+	call KillMotor
+
+	;保存参数(save parameter about pic.bin)
+	;bHasPicBin		2bytes
+	;PIC_SGE		4bytes
+	;PIC_OFFSET		4bytes
+	xor ax,ax
+	mov al,[bHasPicBin]
+	push ax
+	mov eax,PIC_SEG
+	push eax
+	mov eax,PIC_OFFSET
+	push eax
+	jmp LOAD_SEG:LOAD_OFFSET
+
+;***********************procedure**********************
 ;********************increase a sector******************
 IncSector:
 	mov ax,[start]
@@ -67,15 +191,17 @@ IncSector:
 	add ax,200h
 	mov word [copy_off],ax
 	ret
+;***********************procedure*********************
 ;********************read a sector*********************
-;********************读取一个扇区（al=1）*********************
-ReadSector:
-goon:					;write to es:bx
+;********************读取一个扇区（al=1）*************
+;读取的扇区数据会保存到es:bx内存处(the floppy reader will read sector content to the memory es:bx)
+ReadASector:					;write to es:bx
 	mov ax,word [copy_ds]							
 	mov es,ax
 	mov bx,word [copy_off]	;es:bx 0:0x7e00,是在0x7c00后的0x200h
+	;以下是固定阐述
 	mov ah,02h		;ah int 13 function number
-	mov al,01h		;读取扇区数
+	mov al,01h		;读取扇区数(number of sector to read)
 	mov ch,[tracker]	;ch tracker	(从0开始)
 	mov dl,[driver]	;dl driver		(从0开始)	
 	mov dh,[header]	;dh header	(从0开始)
@@ -100,16 +226,65 @@ goon:					;write to es:bx
 	;int 0x13	; call the interrupt
 	;jnc exit	; if the carry flag is clear, it worked
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	jc goon
-
+	jnc NoReadError
+	
+	xor dl,dl
+	xor ah,ah
+	int 0x13
+	jmp ReadASector
+NoReadError:
 	ret
-sector	db	2
-header	db	0	
+;***********************procedure*********************
+;********************打印回车换行*********************
+printEnter:
+	push ax
+	mov ax,0xe0d
+	int 0x10
+	mov ax,0xa
+	int 0x10
+	pop ax
+	ret
+;**********************procedure***********************
+;*********************KillMotor*************************
+KillMotor:
+	push dx
+	mov dx,0x3F2
+	mov al,0
+	out dx,al
+	pop dx
+	ret
+
+
+;***********************变量定义(variable definition)**********************
+;定义扇区相关(defined sectors related)
+sector	db	2	;从第二个扇区开始读
+header	db	0	;
 tracker	db	0
 driver	db	0
-start		dw	1
+start	dw	1
 secPer	db	18
-copy_off	dw  0x7e00
+
+bHasPicBin db 0
+
+BOOT_NEW_SEG equ 0x1000
+BOOT_START_SEG equ 0x7c0
+
+LOAD_SEG equ 0x1000
+LOAD_OFFSET equ 0x400
+
+KERNEL_SEG equ 0x1000
+KERNEL_OFFSET equ 0x600
+
+PIC_SEG equ 0x100
+PIC_OFFSET equ 0
+
+FLOPPY_TEMP_ADDR_OFFSET equ 0x7c00
+FLOPPY_TEMP_ADDR_SEG equ 0
+
+STACK_SEG equ 0
+STACK_OFFSET equ 0x10000
+
+copy_off	dw  0x0
 copy_ds	dw  0x0
 times 510-($-$$) db 0
 dw 0xaa55
--- boot/load.s
+++ boot/load.s
@@ -1,34 +1,80 @@
 ;       loader									
 ;	by shindow(liuyu_200411@sina.com) 					
-;	2010.1.15  setup		  												
+;	2010.1.15  setup		  
+;
+;	rewrite on 214.9.1
+;
 %include "protect.inc"
 [BITS 16]
-mov ax,cs		
-mov ds,ax
-mov ax,0x1020		;pic.bin被加载到0x10200
-mov fs,ax
 
+	;通过对堆栈中的参数来加载pic.bin(load pic.bin by the parameter in stack)
+	mov ax,cs		
+	mov ds,ax
+	mov es,ax
 
-mov ah,0
-mov al,3	 
-;mov al,13h
-int 10h		;03h: 80x25 Text, 16 colors, 8 pages 并清屏
-mov ah,0xb
-mov bh,0
-mov bl,4
-int 10h		;设置背景颜色
-;=========加载图片=========
-mov cx,2000
-mov ax,0xb800
-mov es,ax
-mov si,0
-mov di,0
-L1:
-	mov al,byte [fs:si]
-	inc si
-	mov byte [es:di],al
-	add di,2
-	loop L1
+	;AH=00/INT 10H 是用来设定显示模式的服务程序，AL 寄存器表示欲设定的模式：
+	;AL 	文字/图形 	分辨率 	颜色
+	;00 	文字(text) 	40*25 	2
+	;01 	文字(text) 	40*25 	16
+	;02 	文字(text) 	80*25 	2
+	;03 	文字(text) 	80*25 	16
+	;04 	图形(graphic) 	320*200 	2
+	;05 	图形(graphic) 	320*200 	4
+	;06 	图形(graphic) 	640*200 	2
+	mov ah,0
+	mov al,3	 
+	int 10h
+	;AH=0Bh, BH = 00h
+	;Set background/border color 
+	;BL = Background/Border color (border only in text modes) 	
+	;AH=0Bh, BH = 01h
+	;Set palette 	 	
+	;BL = Palette ID (was only valid in CGA, but newer cards support it in many or all graphics modes)
+	;0 	0000 	Black 	 
+	;1 	0001 	Blue 	
+	;2 	0010 	Green 	
+	;3 	0011 	Cyan 	
+	;4 	0100 	Red 	
+	;5 	0101 	Magenta 	
+	;6 	0110 	Brown 	
+	;7 	0111	 	Light Gray 	
+	;8 	1000 	Dark Gray 	
+	;9 	1001 	Light Blue 	
+	;A 	1010 	Light Green 	
+	;B 	1011 		Light Cyan 	
+	;C 	1100 	Light Red 	
+	;D 	1101 		Light Magenta 	
+	;E 	1110 		Yellow 	
+	;F 	1111 		White
+	mov ah,0xb
+	mov bh,0
+	mov bl,4
+	int 10h		;设置背景颜色
+	;stack:
+	;bHasPicBin		2bytes
+	;PIC_SGE		4bytes
+	;PIC_OFFSET		4bytes
+
+	pop eax
+	pop ebx
+	pop cx
+	
+	cmp cx,0
+	jz NoNeedToLoadPicBin
+
+	mov esi,eax	
+	mov fs,bx
+	mov cx,2000
+	mov ax,0xb800
+	mov es,ax
+	mov di,0
+	L1:
+		mov al,byte [fs:si]
+		inc si
+		mov byte [es:di],al
+		add di,2
+		loop L1
+NoNeedToLoadPicBin:
 ;=======================
 mov dh,11	;行	
 mov dl,0		;列
@@ -142,13 +188,15 @@ Display:
 	pop ax
 	pop bp
 	ret
-;*****************dh:行,dl:列****************
+;********************procedure*******************
+;**************dh:行(row),dl:列(column)***********
 SetCursor:
 	push ax
 	mov ah,2
 	int 10h
 	pop ax
 	ret
+;********************procedure*******************
 ;********************InitKernel*******************
 InitKernel:
 	push ds
@@ -188,6 +236,7 @@ NoRead:
 	jnz Again
 	pop ds
 	ret
+;**********************procedure**********************
 ;*********************DisEnter*************************
 DisEnter:
 	push dx
@@ -198,12 +247,14 @@ DisEnter:
 	int 10h
 	pop dx
 	ret
+;**********************procedure**********************
 ;*********************ReadPos:ax is changed***********
 ReadPos:
 	mov bh,0
 	mov ah,0x3	;dl contains x 
 	int 10h		;dh contains y
 	ret
+;**********************procedure***********************
 ;*********************KillMotor*************************
 KillMotor:
 	push dx
@@ -212,6 +263,8 @@ KillMotor:
 	out dx,al
 	pop dx
 	ret
+
+;**********************variable definition***********************
 msgWel		db "shindow,here we go!Welcome to the os world!"
 mWLength	EQU $-msgWel
 sector	db	2
